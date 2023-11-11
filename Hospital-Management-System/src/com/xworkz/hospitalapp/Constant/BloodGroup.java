fd237b589d3168751eeff3be87e243fa73d23827
MounikaV-21 <mounikafeb99@gmail.com>
2023-11-11 09:33:42 +0530

    commiting wrapper class files

--- Hospital-Management-System/src/com/xworkz/hospitalapp/Constant/BloodGroup.java
+++
@@ -1,5 +0,0 @@
-package com.xworkz.hospitalapp.Constant;
-
-public enum BloodGroup {
-    Apos, Bpos, Aneg, Bneg, ABpos, ABneg, Opos, Oneg;
-}
--- Hospital-Management-System/src/com/xworkz/hospitalapp/Constant/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.xworkz.hospitalapp.Constant;
-
-public enum Gender {
-    male, female, transGender, nonBinary, agender, panGender, genderNeutral;
-}
